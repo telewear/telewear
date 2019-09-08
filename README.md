496c3fa4ab7a2e7cf6d4df3787a9512e5601b110
Narek Torosyan <narek.email@gmail.com>
2019-09-08 15:13:04 +0400

    Upload first proto code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# telewear
-A Telegram client for Samsung watches, written in C#.
+# Telewear
+A Telegram client for Samsung watches, written in C# and Xamarin.
+
+Join the announcement channel - [@telewear](https://t.me/telewear)
+
+
---
+++ Telewear/Telewear/Telewear.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Telewear {
+    public class App : Application {
+        public App() {
+            // The root page of your application
+            MainPage = new ContentPage {
+                Content = new TableView {
+                    Intent = TableIntent.Form,
+                    Root = new TableRoot("Telewear") {
+                        new TableSection ("Telewear") {
+                            new TextCell {
+                                Text = "John Smith",
+                                Detail = "Did you bring the apples?"
+                            },
+                            new TextCell {
+                                Text = "Jane Smith",
+                                Detail = "Happy birthday!"
+                            },
+                            new TextCell {
+                                Text = "Dead Memes",
+                                Detail = "Senator: What's 9+10?"
+                            },
+                            new TextCell {
+                                Text = "Minecrafters",
+                                Detail = "Kyle: aw man"
+                            },
+                            new TextCell {
+                                Text = "Telewear",
+                                Detail = "No messages here..."
+                            },
+                            new TextCell {
+                                Text = "",
+                                Detail = "NYEHHH"
+                            }
+                        },
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart() {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep() {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume() {
+            // Handle when your app resumes
+        }
+    }
+}
