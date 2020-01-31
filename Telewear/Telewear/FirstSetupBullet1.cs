9742fd683f54f9d38a6c5c89005bbd777c3b6337
Narek Torosyan <narek.email@gmail.com>
2020-01-31 15:51:17 +0400

    Create FirstSetupBullet1.cs

---
+++ Telewear/Telewear/FirstSetupBullet1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Telewear {
+    public class FirstSetupBullet1 : ContentPage {
+        public FirstSetupBullet1() {
+            Content = new StackLayout {
+                Children = {
+                    new Label { Text = "Telegram", FontSize=30},
+                    new Label { Text = "it's a messaging app" }
+                }
+            };
+        }
+    }
+}
