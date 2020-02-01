e247fa4143066dd44137488d8e06621c65636701
Narek Torosyan <narek.email@gmail.com>
2020-02-01 18:53:56 +0400

    Add a proper phone number form

--- Telewear/Telewear/OnbEnterPhone.cs
+++ Telewear/Telewear/OnbEnterPhone.cs
@@ -8,10 +8,20 @@ using Xamarin.Forms;
 namespace Telewear {
     public class OnbEnterPhone : ContentPage {
         public OnbEnterPhone(bool newUser) {
-            var dojo = newUser ? "newb" : "Telegram guru";
+            var action = newUser ? "sign you up" : "log you in";
             Content = new StackLayout {
                 Children = {
-                    new Label { Text = "Welcome to Xamarin.Forms," + dojo + "!", HorizontalTextAlignment = TextAlignment.Center }
+                    new Entry {
+                        Margin = new Thickness (20, 70, 20, 10),
+                        Placeholder = "Phone Number"
+                    },
+                    new Label {
+                        Text = "Please enter your phone number so we can " + action + ".",
+                        HorizontalTextAlignment = TextAlignment.Center,
+                    },
+                    new Button {
+                        Text = "Next"
+                    }
                 }
             };
         }
