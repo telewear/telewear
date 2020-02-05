e118d63e6c12a23071575c1d726cba754b72e447
Narek Torosyan <narek.email@gmail.com>
2020-02-05 20:54:56 +0400

    Create User.cs

---
+++ Telewear/Telewear/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telewear {
+    public class User {
+        public string phone;
+        public string name;
+        public string surname;
+        public string bio;
+        public string pic;
+        
+
+    }
+}
