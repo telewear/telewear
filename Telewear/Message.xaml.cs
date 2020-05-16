d521ae1604537ef74337446cebfadeaa6aa7dfd1
Narek Torosyan <narek.email@gmail.com>
2020-05-16 18:54:04 +0400

    Add message control
    It's currently very simple

--- Telewear/Message.xaml.cs
+++ Telewear/Message.xaml.cs
@@ -9,9 +9,35 @@ using Xamarin.Forms.Xaml;
 
 namespace Telewear {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Message : ContentView {
+    public partial class Message : StackLayout {
+
+        public static readonly BindableProperty SenderProperty =
+            BindableProperty.Create(nameof(Sender), typeof(string), typeof(Message),  default(string), BindingMode.OneWay);
+        public string Sender {
+            get { return (string)GetValue(SenderProperty); }
+            set { SetValue(SenderProperty, value); }
+        }
+
+        public static readonly BindableProperty MessageTextProperty =
+            BindableProperty.Create(nameof(MessageText), typeof(string), typeof(Message), default(string), BindingMode.OneWay);
+        public string MessageText {
+            get { return (string)GetValue(MessageTextProperty); }
+            set { SetValue(MessageTextProperty, value); }
+        }
+
         public Message() {
             InitializeComponent();
+        }
+
+        protected override void OnPropertyChanged(string propertyName = null) {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == SenderProperty.PropertyName) {
+                sender.Text = Sender;
+            }
+            else if (propertyName == MessageTextProperty.PropertyName) {
+                message.Text = MessageText;
+            }
         }
     }
 }
