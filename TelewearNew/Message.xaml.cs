ef3748d6a206890da348baa07bf927830f633683
Narek Torosyan <narek.email@gmail.com>
2020-05-16 18:41:03 +0400

    Make Telewear a Tizen XAML app
    Hopefully this fixes some of the problems I had when it was a cross platform Xamarin app (and who needs a dumbed down Telegram for watches for something other than a watch)

---
+++ TelewearNew/Message.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TelewearNew {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Message : ContentView {
+        public Message() {
+            InitializeComponent();
+        }
+    }
+}
