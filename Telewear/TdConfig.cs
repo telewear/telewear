2ed3fc5249847300698798d532898db09d58bc09
Narek Torosyan <narek.email@gmail.com>
2020-07-14 21:18:16 +0400

    Create TdConfig.cs

---
+++ Telewear/TdConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telewear {
+    partial class TdConfig {
+        public static string api_id = "";
+        public static string api_hash = "";
+    }
+}
