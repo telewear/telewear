399ab9cbba2bed6a96f7b9a2830df6fedc5aaf0e
Narek Torosyan <narek.email@gmail.com>
2020-11-30 17:19:12 +0400

    Move demo code

--- Telewear/MainPage.xaml.cs
+++ Telewear/MainPage.xaml.cs
@@ -13,31 +13,40 @@ namespace Telewear {
     public partial class MainPage : CirclePage {
         public MainPage() {
             InitializeComponent();
+        }
+
+        // test code
+        void MessageDemo() {
             Config.scheme = Colors.Dark;
-            stack.Children.Add(new Message {
+            stack.Children.Add(new Message
+            {
                 Sender = "John Smith",
                 MessageText = "Incoming",
                 HorizontalOptions = LayoutOptions.Start
             });
             Config.scheme = Colors.Light;
-            stack.Children.Add(new Message {
+            stack.Children.Add(new Message
+            {
                 Sender = "Jane Smith",
                 MessageText = "Are you a robot?",
                 HorizontalOptions = LayoutOptions.End
             });
             Config.scheme = Colors.SMS;
-            stack.Children.Add(new Message {
+            stack.Children.Add(new Message
+            {
                 Sender = "Tizen Hater",
                 MessageText = "Samsung should die",
                 HorizontalOptions = LayoutOptions.Start
             });
             Config.scheme = Colors.Dark;
-            stack.Children.Add(new Message {
+            stack.Children.Add(new Message
+            {
                 Sender = "Jane Smith",
                 MessageText = "Calm down Tizen Hater.",
                 HorizontalOptions = LayoutOptions.End
             });
-            stack.Children.Add(new Message {
+            stack.Children.Add(new Message
+            {
                 Sender = "John Smith",
                 MessageText = "The Tizen dev tools need to die, not Samsung",
                 HorizontalOptions = LayoutOptions.Start
